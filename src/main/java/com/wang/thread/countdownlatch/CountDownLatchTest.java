9f3e18632983baf78769e2a24a1d8a9e22165f79
kweilin123 <kweilin1029@>
2020-03-10 08:26:25 +0800

    countdownlatch

---
+++ src/main/java/com/wang/thread/countdownlatch/CountDownLatchTest.java
@@ -0,0 +1,20 @@
+package com.wang.thread.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchTest {
+
+	public static void main(String[] args) {
+		CountDownLatch latch = new CountDownLatch(10);
+		for (int i = 0; i < 10; i++) {
+			new CountDownThread(String.valueOf(i), latch).start();
+		}
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("latch all end");
+	}
+
+}
---
+++ src/main/java/com/wang/thread/countdownlatch/CountDownThread.java
@@ -0,0 +1,31 @@
+package com.wang.thread.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CountDownThread extends Thread {
+	private static final Logger logger = LoggerFactory.getLogger(CountDownThread.class);
+	private String name;
+	private CountDownLatch latch;
+
+	public CountDownThread(String name, CountDownLatch latch) {
+		this.name = name;
+		this.latch = latch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep((long) (10000 * Math.random()));
+			latch.countDown();
+			logger.info("CountDownLatch finish" + name);
+			// 执行完可继续往下运行
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
